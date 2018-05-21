d45f5b8fedc68c5fdf2c5022efc86f399a25f3d1
Sinan Akpolat <aciliketcap@gmail.com>
2018-05-21 17:34:27 +0300

    Show a sample picture inside a label

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QImageReader>
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QImageReader r("../photo/sample.jpg");
+    const QImage sampleImg = r.read();
+    if(sampleImg.isNull()) {
+        QMessageBox::information(this,
+                                 QGuiApplication::applicationDisplayName(),
+                                 tr("load failed: %1").arg(r.errorString()));
+    } else {
+        ui->srcImg->setPixmap(QPixmap::fromImage(sampleImg));
+    }
 }
 
 MainWindow::~MainWindow()
